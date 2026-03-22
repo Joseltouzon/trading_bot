8153fe0e93ae22fb68ca547e1e003ebe00b42aa5
Jose Luis Lopez Touzon <josel.touzon@gmail.com>
2026-03-22 17:16:05 -0300

    se agregagn simbolos de structure a config

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ DEFAULT_STRATEGY_SYMBOLS = {
     "stop_hunt":        ["XRPUSDT", "1000PEPEUSDT", "AVAXUSDT", "TIAUSDT", "ORDIUSDT"],
     "macd_momentum":    ["XRPUSDT", "AVAXUSDT", "SOLUSDT", "LINKUSDT", "TIAUSDT"],
     "ema_breakout":     ["DOGEUSDT", "LINKUSDT", "TIAUSDT", "ORDIUSDT", "PENDLEUSDT", "1000BONKUSDT"],
-    "structure_break":  [],
+    "structure_break":  ["FILUSDT", "DOGEUSDT", "APTUSDT", "WIFUSDT", "ATOMUSDT"],
 }
 REQUIRED_INTERVALS = sorted(set(STRATEGY_INTERVALS.values()))  # ["15m", "5m"]
 
